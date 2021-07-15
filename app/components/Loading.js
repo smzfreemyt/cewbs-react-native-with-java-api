37bc83e84e20c647c96da11b738526789b54daca
Zidrex Andag <john.andag@monstar-lab.com>
2021-07-15 10:05:35 +0800

    refactor loading component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Spinner from 'react-native-spinkit';
 import colors from '../utils/colors';
 
