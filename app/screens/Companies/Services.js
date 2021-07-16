9fd336b258ba934c92616f518d73812c5c10ff2a
Rovin Leano <rovin.leano@monstar-lab.com>
2021-07-16 12:19:25 +0800

    Added fetching of company services

--- app/screens/Companies/Services.js
+++ app/screens/Companies/Services.js
@@ -1,16 +1,90 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity, Image} from 'react-native';
 import TopBar from '../../components/TopBar';
+import { Button } from 'react-native-paper';
 
-const Services = ({navigation}) => {
+const Services = ({navigation, route}) => {
+  const [company, setCompany] = useState(route.params.data);
+  console.log(company.data.services)
+  const renderServices = ({item}) => {
+    return (
+      <TouchableOpacity
+        style={styles.yellowBox}
+        >
+        <View style={styles.companyItem}>
+          <Image source={{uri: item.logo}}
+          style={styles.partnerImage}/>
+          <Text style={styles.companyName}>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View>
       <TopBar />
-      <Text onPress={() => navigation.goBack()}>Services</Text>
+      <View style={styles.back}>
+        <Button onPress={() => navigation.goBack()}>{'<< '}Back</Button>
+      </View>
+      <Text style={styles.screenTitle}>{company.data.name}'s Services</Text>
+      <View style={styles.container}>
+        <FlatList
+          data={company.data.services}
+          renderItem={({item}) => renderServices({item}, navigation.navigate)}
+          numColumns={2}
+          keyExtractor={item => item.id}
+        />
+      </View>
     </View>
   );
 };
 
 export default Services;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  back: {
+    textAlign: 'left',
+    width: '25%',
+  },
+  screenTitle: {
+    alignSelf: 'center',
+    fontSize: 25,
+    fontWeight: 'bold',
+    paddingTop: 15,
+    paddingBottom: 20,
+  },
+  container: {
+    padding: 15,
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: 15,
+    paddingBottom: 20,
+  },
+  yellowBox: {
+    backgroundColor: '#FFFF00',
+    flexWrap: 'wrap',
+    marginBottom: 20,
+    width: '50%',
+    marginRight: 20,
+    minHeight: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: 5
+  },
+  companyItem: {
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  partnerImage: {
+    width: 100,
+    height: 50,
+    resizeMode: 'contain'
+  },
+  companyName: {
+    textAlign: 'center',
+    fontWeight: 'bold'
+  }
+});
